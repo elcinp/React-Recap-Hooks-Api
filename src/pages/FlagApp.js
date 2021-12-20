7d2f78fc02cc54afc54a402953900257c92b06e6
elcinp <elcinpars1@gmail.com>
2021-12-20 22:43:23 +0300

    error setError added

--- src/pages/FlagApp.js
+++ src/pages/FlagApp.js
@@ -3,32 +3,48 @@ import React,{useEffect,useState} from 'react'
 const FlagApp = () => {
 
     const[countries,setCountries]=useState([])
+    const [error,setError] =useState(false)
     console.log(countries)
+
     useEffect(() => {
         fetch("https://restcountries.com/v3/all")
-        .then((res) => res.json())
+        // .then((res) => res.json())
+        .then((res) => {
+            if(res.status>=200 && res.status<=299){
+                return res.json();
+            }else{
+                setError(true)
+            }
+        })
         .then((data) =>setCountries(data))
         .catch((err)=>console.log(err));
         
     }, []); //sadece ilk açıldığında çalışacak
 
-
-    return (
-        <div>
-            <h1>Countries</h1>
-            {countries.length}
-            {countries.map((country) => {
-                const {name,capital,flags} = country
-                return(
-                    <div key={name.common}>
-                        <h2>{name.common}</h2>
-                        <img src={flags[1]} alt="" />
-                        <h4>Capital of the {name.common} is {capital}</h4>
-                    </div>
-                )
-            })}
-        </div>
-    )
+    if(!error){
+        return (
+            <div>
+                <h1>Countries</h1>
+                {countries.length}
+                {countries.map((country) => {
+                    const {name,capital,flags} = country
+                    return(
+                        <div key={name.common}>
+                            <h2>{name.common}</h2>
+                            <img src={flags[1]} alt="" />
+                            <h4>Capital of the {name.common} is {capital}</h4>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }else{
+        return(
+            <div>
+                <h1>Error!!</h1>
+            </div>
+        )
+    }
 }
 
 export default FlagApp
